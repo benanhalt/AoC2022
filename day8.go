1d02e2f12306f6cfcadbd831331a6de70748d28c
Ben Anhalt <ben.anhalt@gmail.com>
2022-12-08 23:38:25 -0600

    day 8 part 1.

---
+++ day8.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	//"strconv"
+)
+
+func main() {
+	f, _ := os.ReadFile("input8.txt")
+	lines := strings.Split(string(f), "\n")
+
+	w := len(lines[0])
+	h := len(lines) - 1
+
+	var max byte
+	vis := make(map[string]bool)
+	for r := 0; r < h; r++ {
+		vis[fmt.Sprintf("%d,%d", r, 0)] = true
+		max = lines[r][0]
+		for c := 1; c < w; c++ {
+			if lines[r][c] > max {
+				vis[fmt.Sprintf("%d,%d", r, c)] = true
+				max = lines[r][c]
+			}
+		}
+		vis[fmt.Sprintf("%d,%d", r, w-1)] = true
+		max = lines[r][w-1]
+		for c := w-2; c >= 0; c-- {
+			if lines[r][c] > max {
+				vis[fmt.Sprintf("%d,%d", r, c)] = true
+				max = lines[r][c]
+			}
+		}
+	}
+	for c := 0; c < w; c++ {
+		vis[fmt.Sprintf("%d,%d", 0, c)] = true
+		max = lines[0][c]
+		for r:= 1; r < h; r++ {
+			if lines[r][c] > max {
+				vis[fmt.Sprintf("%d,%d", r, c)] = true
+				max = lines[r][c]
+			}
+		}
+		vis[fmt.Sprintf("%d,%d", h-1, c)] = true
+		max = lines[h-1][c]
+		for r:= h-2; r >= 0; r-- {
+			if lines[r][c] > max {
+				vis[fmt.Sprintf("%d,%d", r, c)] = true
+				max = lines[r][c]
+			}
+		}
+	}
+
+	fmt.Println("Part 1:", w, h, len(vis))
+	for r := 0; r < h; r++ {
+		l := []string{}
+		for c := 0; c < w; c++ {
+			if vis[fmt.Sprintf("%d,%d", r, c)] {
+				l = append(l, "*")
+			} else {
+				l = append(l, " ")
+			}
+		}
+		fmt.Println(strings.Join(l, ""))
+	}
+
+	fmt.Println("Part 2:", )
+}
