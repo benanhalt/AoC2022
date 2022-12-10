ffdd166b75c288c547445512c2b35ed183b0d253
Ben Anhalt <ben.anhalt@gmail.com>
2022-12-10 13:13:32 -0600

    finish day 8.

--- cmd/day8/main.go
+++ cmd/day8/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	//"strconv"
 )
 
 func main() {
@@ -15,56 +14,100 @@ func main() {
 	h := len(lines) - 1
 
 	var max byte
-	vis := make(map[string]bool)
+	vis := make(map[[2]int]bool)
 	for r := 0; r < h; r++ {
-		vis[fmt.Sprintf("%d,%d", r, 0)] = true
+		vis[[2]int{r, 0}] = true
 		max = lines[r][0]
 		for c := 1; c < w; c++ {
 			if lines[r][c] > max {
-				vis[fmt.Sprintf("%d,%d", r, c)] = true
+				vis[[2]int{r, c}] = true
 				max = lines[r][c]
 			}
 		}
-		vis[fmt.Sprintf("%d,%d", r, w-1)] = true
+		vis[[2]int{r, w - 1}] = true
 		max = lines[r][w-1]
-		for c := w-2; c >= 0; c-- {
+		for c := w - 2; c >= 0; c-- {
 			if lines[r][c] > max {
-				vis[fmt.Sprintf("%d,%d", r, c)] = true
+				vis[[2]int{r, c}] = true
 				max = lines[r][c]
 			}
 		}
 	}
 	for c := 0; c < w; c++ {
-		vis[fmt.Sprintf("%d,%d", 0, c)] = true
+		vis[[2]int{0, c}] = true
 		max = lines[0][c]
-		for r:= 1; r < h; r++ {
+		for r := 1; r < h; r++ {
 			if lines[r][c] > max {
-				vis[fmt.Sprintf("%d,%d", r, c)] = true
+				vis[[2]int{r, c}] = true
 				max = lines[r][c]
 			}
 		}
-		vis[fmt.Sprintf("%d,%d", h-1, c)] = true
+		vis[[2]int{h - 1, c}] = true
 		max = lines[h-1][c]
-		for r:= h-2; r >= 0; r-- {
+		for r := h - 2; r >= 0; r-- {
 			if lines[r][c] > max {
-				vis[fmt.Sprintf("%d,%d", r, c)] = true
+				vis[[2]int{r, c}] = true
 				max = lines[r][c]
 			}
 		}
 	}
 
-	fmt.Println("Part 1:", w, h, len(vis))
+	fmt.Println("Part 1:", len(vis))
+	// for r := 0; r < h; r++ {
+	// 	l := []string{}
+	// 	for c := 0; c < w; c++ {
+	// 		if vis[[2]int{r, c}] {
+	// 			l = append(l, "*")
+	// 		} else {
+	// 			l = append(l, " ")
+	// 		}
+	// 	}
+	// 	fmt.Println(strings.Join(l, ""))
+	// }
+
+	best := 0
 	for r := 0; r < h; r++ {
-		l := []string{}
 		for c := 0; c < w; c++ {
-			if vis[fmt.Sprintf("%d,%d", r, c)] {
-				l = append(l, "*")
-			} else {
-				l = append(l, " ")
+			count := 0
+			for cc := c + 1; cc < w; cc++ {
+				count++
+				if lines[r][cc] >= lines[r][c] {
+					break
+				}
+			}
+			score := count
+			count = 0
+
+			for cc := c - 1; cc >= 0; cc-- {
+				count++
+				if lines[r][cc] >= lines[r][c] {
+					break
+				}
+			}
+
+			score *= count
+			count = 0
+			for rr := r + 1; rr < w; rr++ {
+				count++
+				if lines[rr][c] >= lines[r][c] {
+					break
+				}
+			}
+
+			score *= count
+			count = 0
+			for rr := r - 1; rr >= 0; rr-- {
+				count++
+				if lines[rr][c] >= lines[r][c] {
+					break
+				}
+			}
+
+			score *= count
+			if score > best {
+				best = score
 			}
 		}
-		fmt.Println(strings.Join(l, ""))
 	}
-
-	fmt.Println("Part 2:", )
+	fmt.Println("Part 2:", best)
 }
