092e04207dd54242aa7e2e72a86b581962c0b14d
Ben Anhalt <ben.anhalt@gmail.com>
2022-12-11 00:10:27 -0600

    do both parts.

--- cmd/day11/main.go
+++ cmd/day11/main.go
@@ -33,7 +33,11 @@ func monkeyOp(monkey int, old uint64) uint64 {
 func main() {
 	f, _ := os.ReadFile("input.txt")
 	lines := strings.Split(string(f), "\n")
+	part1(lines)
+	part2(lines)
+}
 
+func part1(lines []string) {
 	monkeyItems := [8][]uint64{}
 	monkeyDiv := [8]int{}
 	monkeyTrue := [8]int{}
@@ -64,10 +68,66 @@ func main() {
 			monkeyFalse[monkeyN], _ = strconv.Atoi(strings.TrimSpace(p[len(p)-1]))
 		}
 	}
-	fmt.Println(monkeyItems)
-	fmt.Println(monkeyDiv)
-	fmt.Println(monkeyTrue)
-	fmt.Println(monkeyFalse)
+
+	lcm := uint64(1)
+	for m := 0; m < 8; m++ {
+		lcm *= uint64(monkeyDiv[m])
+	}
+
+	var counts [8]int
+	for round := 0; round < 20; round++ {
+		for m := 0; m < 8; m++ {
+			for _, item := range monkeyItems[m] {
+				counts[m]++
+				worry := monkeyOp(m, item)
+				worry /= 3
+				var throwTo int
+				if worry%uint64(monkeyDiv[m]) == 0 {
+					throwTo = monkeyTrue[m]
+				} else {
+					throwTo = monkeyFalse[m]
+				}
+				monkeyItems[throwTo] = append(monkeyItems[throwTo], worry)
+			}
+			monkeyItems[m] = []uint64{}
+		}
+	}
+
+	sort.Ints(counts[:])
+	fmt.Println("Part 1:", counts[7]*counts[6])
+}
+
+func part2(lines []string) {
+	monkeyItems := [8][]uint64{}
+	monkeyDiv := [8]int{}
+	monkeyTrue := [8]int{}
+	monkeyFalse := [8]int{}
+	monkeyN := -1
+	for _, line := range lines {
+		if strings.HasPrefix(line, "Monkey") {
+			monkeyN++
+		}
+		if strings.HasPrefix(line, "  Starting items") {
+			p := strings.Split(line, ":")
+			pp := strings.Split(p[1], ",")
+			for _, item := range pp {
+				itemN, _ := strconv.Atoi(strings.TrimSpace(item))
+				monkeyItems[monkeyN] = append(monkeyItems[monkeyN], uint64(itemN))
+			}
+		}
+		if strings.HasPrefix(line, "  Test:") {
+			p := strings.Split(line, "by")
+			monkeyDiv[monkeyN], _ = strconv.Atoi(strings.TrimSpace(p[1]))
+		}
+		if strings.HasPrefix(line, "    If true: ") {
+			p := strings.Split(line, " ")
+			monkeyTrue[monkeyN], _ = strconv.Atoi(strings.TrimSpace(p[len(p)-1]))
+		}
+		if strings.HasPrefix(line, "    If false: ") {
+			p := strings.Split(line, " ")
+			monkeyFalse[monkeyN], _ = strconv.Atoi(strings.TrimSpace(p[len(p)-1]))
+		}
+	}
 
 	lcm := uint64(1)
 	for m := 0; m < 8; m++ {
@@ -94,6 +154,5 @@ func main() {
 	}
 
 	sort.Ints(counts[:])
-	fmt.Println("Part 1:", counts[7]*counts[6])
-	fmt.Println("Part 2:")
+	fmt.Println("Part 2:", counts[7]*counts[6])
 }
