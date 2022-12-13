5805b694535f1280e35449ae1ab6c6192276bbf2
Ben Anhalt <ben.anhalt@gmail.com>
2022-12-12 21:37:32 -0600

    clean up and draw maps.

--- cmd/day12/main.go
+++ cmd/day12/main.go
@@ -9,10 +9,9 @@ import (
 )
 
 type state struct {
-	r     int
-	c     int
-	steps int
-	seen  map[[2]int]bool
+	r    int
+	c    int
+	path map[[2]int]bool
 }
 
 func main() {
@@ -37,32 +36,23 @@ func main() {
 	var q deque.Deque
 	var part1, part2 bool
 	seen := make(map[[2]int]bool)
-	q.PushBack(state{r: start[0], c: start[1], steps: 0, seen: map[[2]int]bool{{start[0], start[1]}: true}})
+	q.PushBack(state{r: start[0], c: start[1], path: map[[2]int]bool{{start[0], start[1]}: true}})
 	for i := 0; ; i++ {
 		if q.Len() < 1 {
-			for r := range lines {
-				for c := range lines[0] {
-					if seen[[2]int{r, c}] {
-						fmt.Print(" ")
-					} else {
-						fmt.Print(string(lines[r][c]))
-					}
-				}
-				fmt.Println("")
-			}
+			// no solution
+			render(lines, seen)
 		}
 		s := q.PopFront().(state)
 		seen[[2]int{s.r, s.c}] = true
-		// if i%1000 == 0 {
-		// 	fmt.Println(i, s.r, s.c, string(lines[s.r][s.c]), s.steps, q.Len(), len(seen))
-		// }
 
 		if lines[s.r][s.c] == 'S' && !part1 {
-			fmt.Println("Part 1:", s.steps)
+			fmt.Println("Part 1:", len(s.path)-1)
+			render(lines, s.path)
 			part1 = true
 		}
 		if lines[s.r][s.c] == 'a' && !part2 {
-			fmt.Println("Part 2:", s.steps)
+			fmt.Println("Part 2:", len(s.path)-1)
+			render(lines, s.path)
 			part2 = true
 		}
 		if part1 && part2 {
@@ -78,18 +68,29 @@ func main() {
 				if bb == 'S' {
 					bb = 'z'
 				}
-				// if b == 'n' && bb == 'n' {
-				// 	fmt.Println(s.r, s.c, string(bb), rr, cc, q.Len(), seen[[2]int{rr, cc}])
-				// }
 				if bb >= b-1 && !seen[[2]int{rr, cc}] {
-					// seen := make(map[[2]int]bool)
-					// for k, v := range s.seen {
-					// 	seen[k] = v
-					// }
+					path := make(map[[2]int]bool)
+					for k, v := range s.path {
+						path[k] = v
+					}
 					seen[[2]int{rr, cc}] = true
-					q.PushBack(state{r: rr, c: cc, steps: s.steps + 1, seen: seen})
+					path[[2]int{rr, cc}] = true
+					q.PushBack(state{r: rr, c: cc, path: path})
 				}
 			}
 		}
+	}
+}
+
+func render(lines []string, seen map[[2]int]bool) {
+	for r := range lines {
+		for c := range lines[0] {
+			if seen[[2]int{r, c}] {
+				fmt.Print(" ")
+			} else {
+				fmt.Print(string(lines[r][c]))
+			}
+		}
+		fmt.Println("")
 	}
 }
