a7adf5e818fa452d44c208cee52a3693b1a412c0
Ben Anhalt <ben.anhalt@gmail.com>
2022-12-19 21:54:29 -0600

    day 16 passes example.

--- cmd/day16/main.go
+++ cmd/day16/main.go
@@ -3,28 +3,35 @@ package main
 import (
 	"fmt"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
 )
 
-type room struct {
+const nValves = 60
+
+type Room struct {
 	label   string
 	rate    int
 	tunnels []string
 }
 
-type Args struct {
-	time  int
-	open  string
-	label string
+type State struct {
+	time     int
+	closed   [nValves]string
+	location [2]string
+	opening  [2]bool
+}
+
+type Result struct {
+	amount  int
+	actions []string
 }
 
 func main() {
-	f, _ := os.ReadFile("input.txt")
+	f, _ := os.ReadFile("ex.txt")
 	lines := strings.Split(strings.TrimSpace(string(f)), "\n")
 
-	rooms := make(map[string]room, len(lines))
+	rooms := make(map[string]Room, len(lines))
 	for _, line := range lines {
 		words := strings.Split(line, " ")
 		label := words[1]
@@ -36,53 +43,120 @@ func main() {
 		for _, word := range words[9:] {
 			tunnels = append(tunnels, strings.TrimSuffix(word, ","))
 		}
-		rooms[label] = room{label, rate, tunnels}
+		rooms[label] = Room{label, rate, tunnels}
 		fmt.Println(rooms[label])
 	}
 
-	cache := make(map[Args]result)
-	var optimal func(args Args) result
+	posActions := func(s State, n int) []string {
+		// if s.opening[n] {
+		// 	return []string{"finish"}
+		// }
+		result := []string{}
+		for _, t := range rooms[s.location[n]].tunnels {
+			result = append(result, t)
+		}
+		if contains(s.closed[:], s.location[n]) {
+			result = append(result, "open")
+		}
+		return result
+	}
 
-	optimal = func(args Args) result {
-		if cached, found := cache[args]; found {
+	update := func(s State, as [2]string) State {
+		//fmt.Println("update", s, as)
+		var closed [nValves]string
+		var opening [2]bool
+		var location [2]string
+
+		for i, r := range s.closed {
+			switch {
+			case r == s.location[0] && as[0] == "open":
+				closed[i] = "  "
+			case r == s.location[1] && as[1] == "open":
+				closed[i] = "  "
+			default:
+				closed[i] = s.closed[i]
+			}
+		}
+		for i := range as {
+			switch as[i] {
+			case "open":
+				opening[i] = true
+				location[i] = s.location[i]
+			// case "finish":
+			// 	opening[i] = false
+			// 	location[i] = s.location[i]
+			default:
+				opening[i] = false
+				location[i] = as[i]
+			}
+		}
+		result := State{
+			time:     s.time - 1,
+			closed:   closed,
+			location: location,
+			opening:  opening,
+		}
+		return result
+	}
+
+	added := func(s State, as [2]string) int {
+		amount := 0
+		for i := range as {
+			if as[i] == "open" {
+				amount += rooms[s.location[i]].rate * (s.time - 1)
+			}
+		}
+		return amount
+	}
+
+	cache := make(map[State]Result)
+
+	var optimal func(s State) Result
+	optimal = func(s State) Result {
+		if cached, found := cache[s]; found {
 			return cached
 		}
-		time, open, label := args.time, args.open, args.label
-
-		if time < 1 {
-			return result{0, []string{}}
+		if s.time < 1 {
+			return Result{0, nil}
 		}
-		room := rooms[label]
-
-		var bestPath []string
-		var best int
-		nowOpen := strings.Split(open, " ")
-		if !contains(nowOpen, label) && room.rate > 0 {
-			nowOpen = append(nowOpen, label)
-			sort.Strings(nowOpen)
-
-			for _, tunnel := range room.tunnels {
-				sub := optimal(Args{time - 2, strings.Join(nowOpen, " "), tunnel})
-				amount := sub.amount + room.rate*(time-1)
-				if amount > best {
-					best = amount
-					bestPath = append(sub.path, fmt.Sprint("open", label))
+		best := 0
+		var bestActions []string
+		for _, action0 := range posActions(s, 0) {
+			for _, action1 := range posActions(s, 1) {
+				if action0 == "open" && action1 == "open" && s.location[0] == s.location[1] {
+					continue
+				}
+				actions := [2]string{action0, action1}
+				ns := update(s, actions)
+				sub := optimal(ns)
+				score := sub.amount + added(s, actions)
+				if s.location[0] == "DD" && s.time == 30 {
+					fmt.Println(s.closed, s.time, s.location, s.opening, sub, score)
+				}
+				if score > best {
+					best = score
+					bestActions = append([]string{actions[0] + fmt.Sprint(31-s.time), actions[1]}, sub.actions...)
 				}
 			}
 		}
-		for _, tunnel := range room.tunnels {
-			sub := optimal(Args{time - 1, open, tunnel})
-			if sub.amount > best {
-				best = sub.amount
-				bestPath = append(sub.path, label)
-			}
-		}
-
-		cache[args] = result{best, bestPath}
-		//fmt.Println(args, best, bestPath)
-		return result{best, bestPath}
+		cache[s] = Result{best, bestActions}
+		return cache[s]
 	}
-	fmt.Println(optimal(Args{30, "", "AA"}))
+
+	valves := [nValves]string{}
+	i := 0
+	for l, r := range rooms {
+		if r.rate > 0 {
+			valves[i] = l
+		}
+		i++
+	}
+	fmt.Println(optimal(State{
+		time:     26,
+		location: [2]string{"AA", "AA"},
+		opening:  [2]bool{},
+		closed:   valves,
+	}))
 }
 
 func contains(ss []string, s string) bool {
@@ -93,50 +167,3 @@ func contains(ss []string, s string) bool {
 	}
 	return false
 }
-
-type result struct {
-	amount int
-	path   []string
-}
-
-// func optimal(rooms *map[string]room, cache *map[args]result, time int, label string) result {
-// 	if cached, found := (*cache)[args{time, label}]; found {
-// 		return cached
-// 	}
-
-// 	if time < 1 {
-// 		return result{0, []string{}}
-// 	}
-// 	room := (*rooms)[label]
-
-// 	var bestPath []string
-// 	var best int
-// 	for _, tunnel := range room.tunnels {
-// 		sub := optimal(rooms, cache, time-1, tunnel)
-// 		if sub.amount > best {
-// 			best = sub.amount
-// 			bestPath = append(sub.path, label)
-// 		}
-// 	}
-
-// 	for _, elem := range bestPath {
-// 		if elem == fmt.Sprint("open", label) {
-// 			(*cache)[args{time, label}] = result{best, bestPath}
-// 			fmt.Println(time, label, room.rate, best, bestPath)
-// 			return result{best, bestPath}
-// 		}
-// 	}
-
-// 	for _, tunnel := range room.tunnels {
-// 		sub := optimal(rooms, cache, time-2, tunnel)
-// 		amount := sub.amount + room.rate*(time-1)
-// 		if amount > best {
-// 			best = amount
-// 			bestPath = append(sub.path, fmt.Sprint("open", label))
-// 		}
-// 	}
-
-// 	(*cache)[args{time, label}] = result{best, bestPath}
-// 	fmt.Println(time, label, room.rate, best, bestPath)
-// 	return result{best, bestPath}
-// }
